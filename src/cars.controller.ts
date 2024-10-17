f4daea414704d1e96db66b4a167fe3f91d29d449
karennd <ninodiazkp.7@gmail.com>
2024-10-17 18:07:06 +0000

    added cars controller and added cars component

--- src/cars.controller.ts
+++ src/cars.controller.ts
@@ -4,7 +4,77 @@ import { Controller, Get } from '@nestjs/common';
 export class CarsController {
   @Get()
   getAllCars() {
-    //
-    return { message: 'This is the cars route' };
+    return {
+      year: 2012,
+      brand: 'Chevrolet',
+      model: 'Impala',
+      mileage: '127463',
+      location: 'Tavares',
+      engine: '3.6L V6 DOHC 16V FFV',
+      transmision: '6-Speed Automatic',
+      vin: '2G1WC5E36C1158443',
+      color: 'gold',
+      image:
+        'https://cdn.prod.website-files.com/659d52e325ba52d02c9bab63/670f4c2509ee683b20d2e22f_25de9dc3-0227-40d1-a7f8-d44278655068.jpeg',
+      safety: [
+        'ABS Brakes',
+        'Child Safety Door Locks',
+        'Driver Airbag',
+        'Fog Lights',
+        'Front Side Airbag',
+        'Keyless Entry',
+        'Passenger Airbag',
+        'Rear Window Defogger',
+        'Traction Control',
+        'Vehicle Anti-Theft',
+        'Vehicle Stability Control System',
+      ],
+      comfort: [
+        'Air Conditioning',
+        'AM/FM Radio',
+        'Cruise Control',
+        'Driver Multi-Adjustable Power Seat',
+        'Front Heated Seat',
+        'Manual Sunroof',
+        'Passenger Multi-Adjustable Power Seat',
+        'Power Windows',
+        'Remote Ignition',
+        'Separate Driver/Front Passenger Climate Controls',
+        'Tilt Steering',
+        'Tilt Steering Column',
+      ],
+      exterior: [
+        'Alloy Wheels',
+        'Automatic Headlights',
+        'Daytime Running Lights',
+        'Front Air Dam',
+        'Heated Exterior Mirror',
+        'Rear Spoiler',
+        'Splash Guards',
+      ],
+      interior: [
+        'Cargo Net',
+        'Electrochromic Interior Rearview Mirror',
+        'Leather Seat',
+        'Leather Steering Wheel',
+        'Second Row Folding Seat',
+        'Steering Wheel Mounted Controls',
+        'Trip Computer',
+      ],
+      entertainment: ['CD Player', 'Voice Activated Telephone'],
+      specs: [
+        'Tachometer',
+        'Telematics System',
+        'Tire Pressure Monitor',
+        'Trunk Anti-Trap Device',
+      ],
+      others: [
+        'Interval Wipers',
+        'Power Adjustable Exterior Mirror',
+        'Power Door Locks',
+        'Power Sunroof',
+        'Side Head Curtain Airbag',
+      ],
+    };
   }
 }
--- src/frontend/Car.tsx
+++ src/frontend/Car.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
-// const apiResponse = {};
+import { useEffect, useState } from 'react';
+
 export const Car = () => {
-  return <div className="App">CARS</div>;
+  const [car, setCar] = useState({
+    year: 0,
+    brand: '',
+    model: '',
+    mileage: 0,
+    location: '',
+    image: '',
+    engine: '',
+    transmision: '',
+    vin: '',
+    color: '',
+    safety: [],
+    comfort: [],
+    exterior: [],
+    interior: [],
+    entertainment: [],
+    specs: [],
+    others: [],
+  });
+  useEffect(() => {
+    const fetchCar = async () => {
+      try {
+        const response = await fetch('https://mhqwmz-3005.csb.app/api/cars');
+        const data = await response.json();
+        setCar(data);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    };
+    fetchCar();
+  }, []);
+
+  return (
+    <div className="App">
+      CARS
+      <div>
+        <span>
+          {car.year} / {car.brand} / {car.model}
+        </span>
+        <div>
+          Milleage: {car.mileage} <br /> Location: {car.location} <br />
+        </div>
+        <div>
+          <img width="500" src={car.image} alt="Car Image" />
+        </div>
+        <div>
+          Engine: {car.engine} <br /> Transmision: {car.transmision} <br /> VIN:{' '}
+          {car.vin} <br /> Color: {car.color}
+        </div>
+        <div>
+          Safety: {car.safety} <br /> Comfort: {car.comfort} <br /> Exterior:{' '}
+          {car.exterior} <br /> Interior: {car.interior} <br /> Entertainment:{' '}
+          {car.entertainment} <br /> Specs: {car.specs} <br /> Others:{' '}
+          {car.others}
+          <div />
+        </div>
+        ;
+      </div>
+    </div>
+  );
 };
